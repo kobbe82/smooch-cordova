9d8ea426687a422470729ea4c8dcbc60deef5ee4
Kobbe van Daatselaar <kdaatselaar@zig.nl>
2019-10-03 10:47:28 +0200

    Fix for events

--- src/ios/SmoochCordova.m
+++ src/ios/SmoochCordova.m
@@ -11,11 +11,13 @@
 
 @implementation SmoochCordova
 
+
 - (void)init:(CDVInvokedUrlCommand *)command {
     NSMutableDictionary *settings = [[NSMutableDictionary alloc]
         initWithDictionary:[command argumentAtIndex:0]];
 
     SKTSettings *sktSettingsObj = [[SKTSettings alloc] init];
+
 
     if ([settings valueForKey:@"conversationAccentColor"]) {
         sktSettingsObj.conversationAccentColor = [SmoochCordova colorFromHexString:[settings valueForKey:@"conversationAccentColor"]];
@@ -63,25 +65,33 @@
 }
 
 - (void)conversation:(SKTConversation *)conversation unreadCountDidChange:(NSUInteger)unreadCount {
-    NSString *jsCommand = [NSString stringWithFormat:@"cordova.fireDocumentEvent('smoochconversation', 'unreadCountDidChange', '%lu');", unreadCount];
+    NSString *jsCommand = [NSString stringWithFormat:@"cordova.fireDocumentEvent('smoochconversation', '{type: \'unreadCountDidChange\', details: {unreadCount: \'%lu\'}}');", unreadCount];
     [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
 }
 
 - (void)conversation:(SKTConversation *)conversation didReceiveActivity:(nonnull SKTConversationActivity *)activity {
-   NSString *jsCommand = [NSString stringWithFormat:@"cordova.fireDocumentEvent('smoochconversation', 'didReceiveActivity', '%@');", activity.type];
+   NSString *jsCommand = [NSString stringWithFormat:@"cordova.fireDocumentEvent('smoochconversation', '{type: \'didReceiveActivity\', details: { type: \'%@\'}}');", activity.type];
    [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
 }
 
 - (void)conversation:(SKTConversation *)conversation willShowViewController:(UIViewController *)viewController {
-   [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('smoochconversation', 'willShowViewController');" scheduledOnRunLoop:NO];
+   NSString *jsCommand = [self generateEventCommand:@"willShowViewController" withdata:nil];
+   [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
 }
 
 - (void)conversation:(SKTConversation *)conversation didShowViewController:(UIViewController *)viewController {
-    [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('smoochconversation', 'didShowViewController');" scheduledOnRunLoop:NO];
+    NSString *jsCommand = [self generateEventCommand:@"didShowViewController" withdata:nil];
+    [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
 }
 
 - (void)conversation:(SKTConversation *)conversation didDismissViewController:(nonnull UIViewController *)viewController {
-    [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('smoochconversation', 'didDismissViewController');" scheduledOnRunLoop:NO];
+    NSString *jsCommand = [self generateEventCommand:@"didDismissViewController" withdata:nil];
+    [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
+}
+
+- (void)conversation:(SKTConversation *)conversation willDismissViewController:(nonnull UIViewController *)viewController {
+    NSString *jsCommand = [self generateEventCommand:@"willDismissViewController" withdata:nil];
+    [self.commandDelegate evalJs:jsCommand scheduledOnRunLoop:NO];
 }
 
 #pragma mark - User
@@ -120,6 +130,25 @@
 - (void)sendFailure:(CDVInvokedUrlCommand *)command {
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (NSString *)generateEventCommand:(NSString *)type withdata:(nullable NSDictionary *)data {
+    NSMutableDictionary *eventargument = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *details = [[NSMutableDictionary alloc] init];
+
+    details[@"type"] = type;
+    if(data != nil) {
+        details[@"data"] = data;
+    }
+
+    eventargument[@"detail"] = details;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:eventargument
+      options:0
+      error:nil];
+
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSString *result = [NSString stringWithFormat:@"cordova.fireDocumentEvent('smoochconversation', %@);", jsonString];
+    return result;
 }
 
 // Assumes input like "#00FF00" (#RRGGBB).
